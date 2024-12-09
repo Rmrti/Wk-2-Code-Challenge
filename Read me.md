276b5990161a0cc71029065a57f783189315ffb5
Rmrti <mutuma34@gmail.com>
2024-12-09 15:15:31 +0300

    Creates a shopping list app

---
+++ Read me.md
@@ -0,0 +1,62 @@
+##My Shopping List App
+
+
+An uncomplicated shopping list application made out of HTML and CSS. The app lets you add items along with its price, check off purchased items, and delete items in the list.
+
+##Features:
+
+
+   Add Items: Type in the item name and price to add it to your shopping list.
+
+
+   Mark Purchased: Click the ‘Mark Purchased’ button on the right side of each item to mark it as purchased.
+
+
+   Clear List: To remove everything in the list of items, click on 'Clear All’
+
+
+##Installation:
+
+
+   You can either clone the repository or download the files.
+
+
+   Make sure you have all the files that are:
+
+        
+       index.html - Main HTML file.
+
+
+       Images folder
+
+
+       styles.css – The CSS file which have the styles.
+
+
+       script.js – JavaScript file with the logic of the app.
+
+
+   Launch the app by opening index.html file in your browser.
+
+
+##Usage:
+
+
+   Launch the app inside a browser.
+
+
+   Type in the name and the price of the items you want to add and press ‘Add Item’.
+
+
+   Use ‘Mark Purchased’ button to mark items you have bought.
+
+
+   To remove all items click ‘Clear All’.
+
+
+##License:
+
+
+This is a free to use and modify the project.
+
+
---
+++ script.js
@@ -0,0 +1,69 @@
+// Shopping List App Logic
+
+// DOM element references
+const nameInput = document.querySelector('#item-name');
+const priceInput = document.querySelector('#item-price');
+const addItemBtn = document.querySelector('#add-item');
+const clearListBtn = document.querySelector('#clear-list');
+const listOutput = document.querySelector('#shopping-list');
+
+let listItems = [];
+
+// Function to refresh the list view
+function updateList() {
+  listOutput.innerHTML = '';
+  listItems.forEach(function(item, index) {
+    const listEntry = document.createElement('li');
+    listEntry.classList.add('list-item');
+    
+    // Create the list item content
+    listEntry.innerHTML = `
+      <span>${item.name} - Ksh ${item.price}</span>
+      <button class="mark-btn" data-index="${index}">Mark Purchased</button>
+    `;
+    
+    // Mark as purchased visually
+    if (item.purchased) {
+      listEntry.classList.add('purchased');
+    }
+    
+    // Append the new list item
+    listOutput.appendChild(listEntry);
+  });
+
+  // Add event listeners to mark items as purchased
+  const markBtns = document.querySelectorAll('.mark-btn');
+  markBtns.forEach(function(button) {
+    button.addEventListener('click', function() {
+      const itemIndex = button.getAttribute('data-index');
+      togglePurchased(itemIndex);
+    });
+  });
+}
+
+// Function to add a new item
+addItemBtn.addEventListener('click', function() {
+  const name = nameInput.value.trim();
+  const price = priceInput.value.trim();
+
+  if (name && price) {
+    listItems.push({ name: name, price: price, purchased: false });
+    updateList();
+    nameInput.value = '';
+    priceInput.value = '';
+  } else {
+    alert('Both name and price are required!');
+  }
+});
+
+// Function to toggle the purchased status
+function togglePurchased(index) {
+  listItems[index].purchased = !listItems[index].purchased;
+  updateList();
+}
+
+// Function to clear the entire list
+clearListBtn.addEventListener('click', function() {
+  listItems = [];
+  updateList();
+});
